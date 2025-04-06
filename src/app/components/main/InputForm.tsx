06ab430b2804ca2db7632aae1af0def852519d57
Byeonghyeon <problemp123@gmail.com>
2025-04-06 17:18:45 +0900

    [feat] 이미지 업로드 버튼

--- src/app/components/main/InputForm.tsx
+++ src/app/components/main/InputForm.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { CiCirclePlus } from 'react-icons/ci'
 
 function InputForm() {
   return (
-    <div>
-        <input type='file'/>
+    <div className='flex flex-col gap-4'>
+        <label htmlFor="image">
+            <div className='flex w-[400px] h-[60px] cursor-pointer hover:bg-neutral-900 transition duration-300 ease-in-out bg-neutral-600 rounded-2xl text-white text-center items-center justify-center'>
+                <CiCirclePlus size={60}/>
+            </div>
+        </label>
+        <input type='file' name='image' id='image' className='hidden' accept="image/*"/>
+        <div>
+            <p className='text-sm font-bold'>현재 설정 : </p>
+        </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import InputForm from "./components/main/InputForm";
 import Title from "./components/main/Title";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center md:m-24 m-48">
+    <div className="flex items-center justify-center md:m-24 m-48 flex-col gap-24">
       <Title />
+      <InputForm />
     </div>
   );
 }
