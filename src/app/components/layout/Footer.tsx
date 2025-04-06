774ef0e62cb743cf8f4b9674d156e8786b1671ca
Byeonghyeon <problemp123@gmail.com>
2025-04-06 22:26:46 +0900

    [chore] footer

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -3,9 +3,9 @@ import Navigator from './Navigator'
 
 function Footer() {
   return (
-    <section className='fixed h-[100px] w-full flex flex-row justify-between items-center p-4 bottom-0 left-0 right-0'>
+    <footer className='fixed h-[100px] w-full flex flex-row justify-between items-center p-4 bottom-0 left-0 right-0'>
         <Navigator />
-    </section>
+    </footer>
   )
 }
 
